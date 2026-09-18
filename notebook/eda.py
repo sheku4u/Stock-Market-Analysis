21edfc161c9f6bcf4f7ba0d32a8153005c7440c2
sheku4u <abhishekbansal27087@gmail.com>
2026-09-19 01:39:47 +0530

    day  1 of project

---
+++ notebook/eda.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import datetime as dt
+
+
+all_list = pd.read_excel(r"data\Abhishek_01-01-2021_14-09-2026.xlsx", sheet_name=None)
+
+
+print(all_list.items())
